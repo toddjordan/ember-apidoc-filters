17d8bc082f5c84edf80e7e366f6e10e17f29bd17
Todd Jordan <todd.jordan@inin.com>
2016-03-09 23:21:46 -0500

    support deprecated

--- ember-yuidoc-filters.js
+++ ember-yuidoc-filters.js
@@ -1,11 +1,18 @@
 
+function isPrivateClass(item) {
+  return item.hasOwnProperty('access') && item.access === 'private';
+};
+
+function isDeprecatedClass(item) {
+  return item.hasOwnProperty('deprecated') && item.deprecated === true;
+};
+
 function isClassToBeIncluded(item, options) {
-  if (options["exclude-class-accessors"]) {
-    var accessors = [].concat(options["exclude-class-accessors"]);
+  if (options["ember-yuidoc-exclude"]) {
+    var accessors = options['ember-yuidoc-exclude'];
     for (var i = 0; i < accessors.length; i++) {
-      if (item.hasOwnProperty('access') &&
-          item.hasOwnProperty('name') &&
-          item.access === accessors[i]) {
+      if ((accessors[i] === 'private' && isPrivateClass(item)) ||
+          (accessors[i] === 'deprecated' && isDeprecatedClass(item))) {
         return false;
       }
     }
@@ -13,9 +20,18 @@ function isClassToBeIncluded(item, options) {
   return true;
 };
 
+function contains(array, item) {
+  for(var i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return true;
+    }
+  }
+  return false;
+};
+
 module.exports = function (data, options) {
-  if (options["exclude-class-accessors"]) {
-    console.log("Excluding classes with accessors: "+ [].concat(options["exclude-class-accessors"]));
+  if (options["ember-yuidoc-exclude"]) {
+    console.log("Excluding classes with accessors: "+ options["ember-yuidoc-exclude"]);
   }
 
   var classesToDocument = {};
@@ -32,6 +48,16 @@ module.exports = function (data, options) {
     classNamesToDocument.push(n);
   }
 
+  for (var namespace in data.modules) {
+    var namespaceClasses = {};
+    var originalClasses = data.modules[namespace].classes;
+    for(var className in originalClasses) {
+      if (contains(classNamesToDocument, className)) {
+        namespaceClasses[className] = 1;
+      }
+    }
+    data.modules[namespace].classes = namespaceClasses;
+  }
+
   data.classes = classesToDocument;
-  data.modules.ember.classes = classNamesToDocument;
 }
